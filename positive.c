3772436c520454eeb8dc56fd2a45277b875ad8b0
thilakrajmurugesan <thilakraj.murugesan@gmail.com>
2017-06-28 15:34:30 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,13 +1,12 @@
- +#include<stdio.h>
-+// checking the nature of a number
-+void main()
-+{
-+ int num;
-+ scnaf("%d",&num);
-+ if(num>0)
-+ printf("positive\n");
-+ else if(num<0)
-+ printf("negative\n");
-+ else
-+ printf("zero\n");
-+}
+ #include<stdio.h>
+void main()
+{
+ int num;
+ scnaf("%d",&num);
+ if(num>0)
+ printf("positive\n");
+ else if(num<0)
+ printf("negative\n");
+ else
+ printf("zero\n");
+}
