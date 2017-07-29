6e99a4484b21ba53363d0a6dd767cb614d2bd458
thilakrajmurugesan <thilakraj.murugesan@gmail.com>
2017-07-29 10:29:11 +0530

    Update and rename alpha.c to alpha1.c

---
+++ alpha1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    int i=0;
+    char str[]="c++";
+    while(str[i])
+    {
+         if(alpha(str[i])
+          printf("character c is an alphabetic\n", str[i] );
+       else
+        printf("character c is not an alphabetic\n",str[i]);
+           }
+    return 0;
+}
