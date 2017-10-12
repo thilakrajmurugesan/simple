e55239d61feb5e5298b142e78e99c33841fe1ab7
thilakrajmurugesan <thilakraj.murugesan@gmail.com>
2017-10-12 17:12:09 +0530

    Create power1.java

---
+++ power1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class power
+{
+public static void main(String args[])
+{
+int num;
+int pow;
+int power;
+Scanner s=new Scanner(System.in);
+ System.out.println("Enter the number");
+ num=s.nextInt();
+ System.out.println("enter the power");
+ pow=s.nextInt();
+System.out.println(power(num,pow);
+}
+public Static power(int a, int b)
+{
+int power=1
+for(int c=0; c<b;c++)
+power*=a;
+return power;}
