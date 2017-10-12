d300d5deace1861c149ac89c1c4eda5ebdc6eddd
thilakrajmurugesan <thilakraj.murugesan@gmail.com>
2017-10-12 16:51:11 +0530

    Create Sum1.c

---
+++ Sum1.c
@@ -0,0 +1,14 @@
+int main()
+{
+  int Number, i, Sum = 0;
+  
+  printf("\nPlease Enter any Integer Value\n");
+  scanf("%d", &Number);
+  
+  for(i = 1; i <= Number; i++)
+  {
+     Sum = Sum + i;
+  }
+  
+  printf("Sum of Natural Numbers = %d", Sum);
+  return 0;
